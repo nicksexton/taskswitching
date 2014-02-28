07d6ace642562a8c5b7e81274ade590a7ddc4b1e
Nick Sexton <nick.sexton@gmail.com>
2014-02-28 12:46:45 +0000

    minor rename, added files

---
+++ pdp_activation_funcs.c
@@ -0,0 +1,21 @@
+double act_gs(double net_input, double old_activation,
+              double step_size, double act_max, double act_min) {
+
+  /* TODO - make activation function generic, so that a variety of
+     functions can be implmented*/
+
+    double eta;
+    if (net_input > 0) {
+        eta = net_input * (act_max - old_activation);
+    }
+    else {
+        if (net_input < 0) {
+            eta = net_input * (old_activation - act_min);
+        }
+        else
+            eta = 0;
+    }
+
+    /*    printf ("activation function returning\n"); */    
+    return  old_activation + (step_size * eta);
+}
---
+++ pdp_activation_funcs.h
@@ -0,0 +1,2 @@
+double act_gs(double net_input, double old_activation,
+              double step_size, double act_max, double act_min);
