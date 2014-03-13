9200f4aae1d2d682256b0656ebb78ff1b00c1711
Nick Sexton <nick.sexton@gmail.com>
2014-03-13 17:53:05 +0000

    forgot header

---
+++ pdpgui.h
@@ -0,0 +1,22 @@
+// definitions for structs used to communicate between callback functions
+#ifndef pdpgui_h
+#define pdpgui_h
+
+typedef struct pdp_simulation {
+
+  pdp_model *model; // head of a possible list of models
+
+  gsl_rng * random_generator;
+
+  // should make params and subject(s) generic
+  gs_stroop_params *params;
+
+  subject_popn *subjects;
+
+  // current state of the simulation
+  int current_subject;
+  int current_trial;
+
+} PdpSimulation;
+
+#endif
