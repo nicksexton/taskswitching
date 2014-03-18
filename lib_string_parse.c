22f3d2d310f3161748012c7eaa333441bcb75b93
Nick Sexton <nick.sexton@gmail.com>
2014-03-18 21:54:51 +0000

    tidy;

--- lib_string_parse.c
+++ lib_string_parse.c
@@ -11,78 +11,7 @@
 #include "lib_string_parse.h"
 
 
-/*
-void init_generic_parameter (GenericParameter * param) {
 
-  // init default parameter settings here
-  param->id = 0;
-  strcpy (param->name, "blank");
-  param->data_int_1 = 0;
-  param->data_int_2 = 0;
-  param->data_int_3 = 0;
-  param->data_double_1 = 0.0;
-  param->data_double_2 = 0.0;
-  param->data_double_3 = 0.0;
-  strcpy (param->data_text_1, "");
-  strcpy (param->data_text_2, "");
-
-}
-*/
-
-/*
-void init_generic_parameter_set (GenericParameterSet *param_set) {
-
-  init_generic_parameter (&(param_set->parameter_1));
-  init_generic_parameter (&(param_set->parameter_2));
-  init_generic_parameter (&(param_set->parameter_3));
-  init_generic_parameter (&(param_set->parameter_4));
-  init_generic_parameter (&(param_set->parameter_5));
-
-
-  param_set->parameter_1.id = 1;
-  strcpy (param_set->parameter_1.name, "NUMERIC_VAR1");
-
-  param_set->parameter_2.id = 2;
-  strcpy (param_set->parameter_2.name, "NUMERIC_VAR2");
-
-  param_set->parameter_3.id = 3;
-  strcpy (param_set->parameter_3.name, "NUMERIC_VAR3");
-
-  param_set->parameter_4.id = 4;
-  strcpy (param_set->parameter_3.name, "TEXT_VAR1");
-
-  param_set->parameter_5.id = 5;
-  strcpy (param_set->parameter_3.name, "TEXT_VAR2");
-
-
-}
-*/
-
-/*
-void print_generic_parameter (GenericParameter * param) {
-
-  printf ("\nParameter %s, id %d:\n", param->name, param->id);
-  printf ("Int data:\t1: %d\t2: %d\t 3: %d\n", 
-	  param->data_int_1, param->data_int_2, param->data_int_3);
-  printf ("Double data:\t1: %4.2f\t2: %4.2f\t 3: %4.2f\n", 
-	  param->data_double_1, param->data_double_2, param->data_double_3);
-  printf ("Text data:\t1: %s\t2: %s\n", 
-	  param->data_text_1, param->data_text_2);
-
-}
-*/
-
-/*
-void print_generic_parameter_set (GenericParameterSet * param) {
-
-  print_generic_parameter (&(param->parameter_1));
-  print_generic_parameter (&(param->parameter_2));
-  print_generic_parameter (&(param->parameter_3));
-  print_generic_parameter (&(param->parameter_4));
-  print_generic_parameter (&(param->parameter_5));
-
-}
-*/
 
 
 // GTK function - uses a treestore
@@ -109,79 +38,7 @@ void pdp_file_segmented_line_to_treestore (int max_fields,
 }
 
 
-/*
-bool pdp_file_parse_segmented_line (int max_fields, 
-				    int field_size, 
-				    char extracted_fields[max_fields][field_size],
-				    GenericParameterSet * destination) {
-  // this function is program specific
-  // for a non-example program, break these out to separate functions
-  // and put the whole thing in a separate import translator file
 
-  // THIS CODE (!strcmp) IS SCREWY!!!   
-  // decide how to process line based on field 0
-  if (!strcmp (extracted_fields[0], "NUMERIC_VAR1")) {
-    
-    if (strcmp (extracted_fields[1], "") != 0) {
-      destination->parameter_1.data_int_1 = atoi (extracted_fields[1]);
-      if (strcmp (extracted_fields[2], "") != 0) {
-	destination->parameter_1.data_int_2 = atoi (extracted_fields[2]);
-      }
-    }
-    return true;
-  }
-
-
-  else if (!strcmp (extracted_fields[0], "NUMERIC_VAR2")) {
-    
-    if (strcmp (extracted_fields[1], "") != 0) {
-      destination->parameter_2.data_int_1 = atoi (extracted_fields[1]);
-      if (strcmp (extracted_fields[2], "") != 0) {
-	destination->parameter_2.data_int_2 = atoi (extracted_fields[2]);
-      }
-    }
-    return true;
-  }
-
-
-  else if (!strcmp (extracted_fields[0], "NUMERIC_VAR3")) {
-    
-    if (strcmp (extracted_fields[1], "") != 0) {
-      destination->parameter_3.data_int_1 = atoi (extracted_fields[1]);
-      if (strcmp (extracted_fields[2], "") != 0) {
-	destination->parameter_3.data_int_2 = atoi (extracted_fields[2]);
-      }
-    }
-    return true;
-  }
-
-
-  else if (!strcmp (extracted_fields[0], "TEXT_VAR1")) {
-    
-    if (strcmp (extracted_fields[1], "") != 0) {
-      strcpy (destination->parameter_4.data_text_1, extracted_fields[1]); 
-      if (strcmp (extracted_fields[2], "") != 0) {
-	strcpy (destination->parameter_4.data_text_2, extracted_fields[2]); 
-      }
-    }
-    return true;
-  }
-
-  else if (!strcmp (extracted_fields[0], "TEXT_VAR2")) {
-    
-    if (strcmp (extracted_fields[1], "") != 0) {
-      strcpy (destination->parameter_5.data_text_1, extracted_fields[1]);
-      if (strcmp (extracted_fields[2], "") != 0) {
-	strcpy (destination->parameter_5.data_text_2, extracted_fields[2]); 
-      }
-    }
-    return true;
-  }
-
-
-  return false;
-}
-*/
 
 
 int pdp_file_segment_new_line (FILE *input_file, 
@@ -265,65 +122,7 @@ int pdp_file_segment_new_line (FILE *input_file,
   }
 }
 
-/*
-// example code to test string_parse functions 
-// change GenericParameterSet type to a program-specific struct containing parameters
-int parse_file (FILE *config_file, GenericParameterSet *my_params) {
 
-
-  char fields [MAX_FIELDS][FIELD_SIZE];
-  int line_counter = 0;
-  int fields_extracted;
-  bool more_lines = true;
-
-  while (more_lines) {
-    line_counter ++;
-    fields_extracted = pdp_file_segment_new_line (config_file, 
-						MAX_FIELDS, 
-						FIELD_SIZE, 
-						fields);
-
-    switch (fields_extracted) {
-    case -2: {
-      printf ("File read error, exiting!\n");
-      exit(EXIT_FAILURE);
-    }
-  
-    case -1: {
-      printf ("End of file reached, no more lines to get\n");
-      more_lines = false;
-      break;
-    }
-
-    default: {
-      printf ("processing line %d\t", line_counter);
-      // process the data
-      if (fields_extracted > 0) { // was any data extracted?
-	// if so, process the data
-
-	if (pdp_file_parse_segmented_line (MAX_FIELDS, 
-					   FIELD_SIZE, 
-					   fields,
-					   my_params)) {
-	  printf ("line imported\n");
-	}
-	else {
-	  printf ("warning! line not understood, not imported\n");
-	}
-
-	// else do nothing
-      }
-      else {
-	printf ("blank line or comment, ignoring\n");
-      }
-    }
-    }
-
-  }
-
-  return 0;
-}
-*/
 
 // more general code - parses an entire config file and transfers first two fields
 // on each line to a to treestore
@@ -380,51 +179,3 @@ int pdp_file_parse_to_treestore (FileData *file_info) {
   return 0;
 }
 
-
-
-/*
-int main () {
-
-
-  FILE *config_file;
-
-  config_file = fopen("gtk_config_file.conf", "r");
-  if (config_file == NULL) {
-    printf ("error! gtk_config_file.conf not opened!\n");
-    return 1;
-  }
-
-  GenericParameterSet my_params;
-  init_generic_parameter_set (&my_params);
-  
-  my_params.parameter_1.id = 1;
-  strcpy (my_params.parameter_1.name, "NUMERIC_VAR1");
-
-  my_params.parameter_2.id = 2;
-  strcpy (my_params.parameter_2.name, "NUMERIC_VAR2");
-
-  my_params.parameter_3.id = 3;
-  strcpy (my_params.parameter_3.name, "NUMERIC_VAR3");
-
-  my_params.parameter_4.id = 4;
-  strcpy (my_params.parameter_3.name, "TEXT_VAR1");
-
-  my_params.parameter_5.id = 5;
-  strcpy (my_params.parameter_3.name, "TEXT_VAR2");
-
-
-  printf ("starting parameter settings:\n");
-  print_generic_parameter_set (&my_params);
-  
-
-  parse_file (config_file, &my_params);
-
-
-  printf ("new parameter settings:\n");
-  print_generic_parameter_set (&my_params);
-
-  return 0;
-
-}
-
-*/
