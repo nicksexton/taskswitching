060280c031d1645a628cec1664e3f4cb48a876de
Nick Sexton <nick.sexton@gmail.com>
2014-04-24 20:05:39 +0100

    added function to write line (trial) to a file

--- simulations/sim_1_itemspecific.py
+++ simulations/sim_1_itemspecific.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python
 
-f = open ("sim_1_test.txt", "a") # opens file for appending
-f.write ("and can I get some pickles on that\n")
-f.close()
+# simulation 1 - item-specific priming effects
+# replicating effects in experiment 5 of Allport & Wylie 2000
+# straight replication of G&S simulation, see Gilbert & Shallice 2002
+
+
+
+def write_trial (block_id, trial_id, wordin, colourin, task, param1, param2):
+    f = open ("sim_1_test.txt", "a") # opens file for appending
+    f.write (str(block_id))
+    f.write ("\t")
+
+    f.write (str(trial_id))
+    f.write ("\t")
+
+    f.write (str(wordin))
+    f.write ("\t")
+
+    f.write (str(colourin))
+    f.write ("\t")
+
+    f.write (str(task))
+    f.write ("\t")
+
+    f.write (param1)
+    f.write ("\t")
+
+    f.write (param2)
+    f.write ("\t\n")
+
+    f.close()
+
+
+write_trial (0, 1, 1, 2, 0, "HebP=1", "")
