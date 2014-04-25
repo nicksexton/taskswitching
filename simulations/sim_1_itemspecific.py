a49c1b29511e784c2d2e2e8ee0974d7d759851ac
Nick Sexton <nick.sexton@gmail.com>
2014-04-25 23:49:13 +0100

    created trials conf file for simulation 1

--- simulations/sim_1_itemspecific.py
+++ simulations/sim_1_itemspecific.py
@@ -5,10 +5,12 @@
 # straight replication of G&S simulation, see Gilbert & Shallice 2002
 
 
+num_blocks = 100 # number of times to run each sequence type
+
 
 def write_trial (block_id, trial_id, wordin, colourin, task, param1, param2):
-    f = open ("sim_1_test.txt", "a") # opens file for appending
-    f.write (str(block_id))
+    f = open ("sim_1_trials.conf", "a") # opens file for appending
+    f.write (block_id)
     f.write ("\t")
 
     f.write (str(trial_id))
@@ -32,4 +34,32 @@ def write_trial (block_id, trial_id, wordin, colourin, task, param1, param2):
     f.close()
 
 
-write_trial (0, 1, 1, 2, 0, "HebP=1", "")
+
+# first write ABCD trials (tasks CCWW) - baseline
+for block in range(0, num_blocks):
+    block_name = 'ABCD_' + str(block) 
+
+    write_trial (block_name, 0, 0, 1, 1, "HebP=1", "") 
+    write_trial (block_name, 1, 1, 2, 1, "HebP=1", "") 
+    write_trial (block_name, 2, 2, 0, 0, "HebP=1", "") 
+    write_trial (block_name, 3, 0, 1, 0, "HebP=1", "") 
+
+
+# next, write ABBC trials (tasks CCWW)
+for block in range(0, num_blocks):
+    block_name = 'ABBC_' + str(block)
+
+    write_trial (block_name, 0, 0, 1, 1, "HebP=2", "") 
+    write_trial (block_name, 1, 1, 2, 1, "HebP=2", "") 
+    write_trial (block_name, 2, 1, 2, 0, "HebP=2", "") 
+    write_trial (block_name, 3, 2, 0, 0, "HebP=2", "") 
+
+
+# next, write ABCB trials
+for block in range(0, num_blocks):
+    block_name = 'ABBC_' + str(block)
+
+    write_trial (block_name, 0, 0, 1, 1, "HebP=2", "") 
+    write_trial (block_name, 1, 1, 2, 1, "HebP=2", "") 
+    write_trial (block_name, 2, 2, 0, 0, "HebP=2", "") 
+    write_trial (block_name, 3, 1, 2, 0, "HebP=2", "") 
