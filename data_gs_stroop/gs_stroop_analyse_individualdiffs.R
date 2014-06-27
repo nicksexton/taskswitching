93fd1a42a1a95b231ca86ababc6032e0774b5a2c
Nick Sexton <nick.sexton@gmail.com>
2014-06-27 11:11:43 +0100

    tidying up individual diffs graphs for transfer for status

--- data_gs_stroop/gs_stroop_analyse_individualdiffs.R
+++ data_gs_stroop/gs_stroop_analyse_individualdiffs.R
@@ -14,22 +14,39 @@ indiffData<- read.delim("gs_stroop_data.txt")
 
 # SCATTER PLOTS
 scatter.tdwt.ri <- ggplot(indiffData, aes(TDwt_i, RI, colour=result))
-scatter.tdwt.ri + geom_point() + geom_smooth(method=lm, aes(fill=result), alpha=0.1) + labs(x = "Task Demand -> Output units inhib. weight", y = "Stroop Interference (RTi - RTc)") + ylim(-10, 100)
+scatter.tdwt.ri +
+  geom_point() +
+  geom_smooth(method=lm, aes(fill=result), alpha=0.1) +
+  labs(x = "Task Demand -> Output units inhib. weight", y = "Stroop Interference (RTi - RTc)") +
+  ylim(-10, 100)
 
 scatter.tdwt.tswc <- ggplot(indiffData, aes(TDwt_i, TSwc, colour=result))
-scatter.tdwt.tswc + geom_point() + geom_smooth(method=lm, aes(fill=result), alpha=0.1) + labs(x = "Task Demand -> Output units inhib. weight", y = "Task Switch Cost (word -> colour)") + ylim(-20, 20)
+scatter.tdwt.tswc +
+  geom_point() +
+  geom_smooth(method=lm, aes(fill=result), alpha=0.1) +
+  labs(x = "Task Demand -> Output units inhib. weight", y = "Task Switch Cost (word -> colour)") +
+  ylim(-20, 20)
 
 
 scatter.tdwt.tscw <- ggplot(indiffData, aes(TDwt_i, TScw, colour=result))
-scatter.tdwt.tscw + geom_point() + geom_smooth(method=lm, aes(fill=result), alpha=0.1) + labs(x = "Task Demand -> Output units inhib. weight", y = "Task Switch Cost (colour -> word)") 
+scatter.tdwt.tscw +
+  geom_point() +
+  geom_smooth(method=lm, aes(fill=result), alpha=0.1) +
+  labs(x = "Task Demand -> Output units inhib. weight", y = "Task Switch Cost (colour -> word)") 
 
 
 scatter.ri.tswc <- ggplot(indiffData, aes(RI, TSwc, colour=result))
-scatter.ri.tswc + geom_point() + geom_smooth(method=lm, aes(fill=result), alpha=0.1) + labs(x = "Stroop Interference (RTi - RTc)", y = "Task Switch Cost (word -> colour)") +xlim (-10, 100) + ylim(-20, 20)
+scatter.ri.tswc +
+  geom_point() + geom_smooth(method=lm, aes(fill=result), alpha=0.1) +
+  labs(x = "Stroop Interference (RTi - RTc)", y = "Task Switch Cost (word -> colour)") +
+  xlim (-10, 100) +
+  ylim(-20, 20)
 
 
 scatter.ri.tscw <- ggplot(indiffData, aes(RI, TScw, colour=result))
-scatter.ri.tscw + geom_point() + geom_smooth(method=lm, aes(fill=result), alpha=0.1) + labs(x = "Stroop Interference (RTi - RTc)", y = "Task Switch Cost (colour -> word)") 
+scatter.ri.tscw +
+  geom_point() + geom_smooth(method=lm, aes(fill=result), alpha=0.1) +
+  labs(x = "Stroop Interference (RTi - RTc)", y = "Task Switch Cost (colour -> word)") 
 
 
 # CORRELATIONS
