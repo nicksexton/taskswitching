1daf21b5c0ca54ce609914800aff85194165f69c
Nick Sexton <nick.sexton@gmail.com>
2014-10-15 16:57:25 +0100

    added run permissions

---
+++ simulations/sim_5_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./sim_3_clean.sh
+./sim_5_clean.sh
+./sim_3_n2rc.py -n 200
+
+mv sim_3_trials.conf sim_5_trials.conf
+mv sim_3_lookup.txt sim_5_lookup.txt
+
+../3task_basic_koch_conflict -t sim_5_trials.conf -m sim_5_model_default.conf
+
+mv 3task_act.txt sim_5_act.txt
+mv 3task_data.txt sim_5_data.txt
