327f70f6c860c433928acd29b9b9ebc1332543c4
Nick Sexton <nick.sexton@gmail.com>
2014-10-16 10:11:15 +0100

    re-written overwritten sim_4 script file

--- simulations/sim_4_run.sh
+++ simulations/sim_4_run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-./sim_3_clean.sh
-./sim_5_clean.sh
-./sim_3_n2rc.py -n 500
+./sim_4_clean.sh
 
-mv sim_3_trials.conf sim_5_trials.conf
-mv sim_3_lookup.txt sim_5_lookup.txt
+./sim_4_asymmetric.py -n 500
 
-#../3task_basic -t sim_4_trials.conf -m sim_4_model_asymmetric.conf
 
-mv 3task_act.txt sim_5_act.txt
-mv 3task_data.txt sim_5_data.txt
+../3task_basic -t sim_4_trials.conf -m sim_4_model_asymmetric.conf
+
+mv 3task_act.txt sim_4_act.txt
+mv 3task_data.txt sim_4_data.txt
