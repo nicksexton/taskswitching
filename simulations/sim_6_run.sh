2fb5320696f1023c5e671ffd5e6ca4efe457e042
Nick Sexton <nick.sexton@gmail.com>
2014-10-28 16:31:53 +0000

    trivial changes to run scripts

--- simulations/sim_6_run.sh
+++ simulations/sim_6_run.sh
@@ -14,12 +14,12 @@ rm sim_6_act.txt
 
 rm sim_6_log.txt
 
-./sim_4_asymmetric.py -n 1000
+./sim_4_asymmetric.py -n 100
 mv sim_4_trials.conf sim_6_trials.conf
-mv sim_4_lookup.txt sim_6_lookup.txt
+mv sim_4_lookup.txt sim_6_lookup_small.txt
 
-../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_asymmetric.conf > sim_6_log.txt
+../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_asymmetric.conf > sim_6_log_small.txt
 # ../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_symmetric.conf > sim_6_log.txt # test conf file
 
-mv 3task_act.txt sim_6_act.txt
-mv 3task_data.txt sim_6_data.txt
+mv 3task_act.txt sim_6_act_small.txt
+mv 3task_data.txt sim_6_data_small.txt
--- simulations/sim_6_run_short.sh
+++ simulations/sim_6_run_short.sh
@@ -18,8 +18,9 @@ rm sim_6_log.txt
 mv sim_4_trials.conf sim_6_trials.conf
 mv sim_4_lookup.txt sim_6_lookup_small.txt
 
-../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_asymmetric.conf > sim_6_log_small.txt
-# ../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_symmetric.conf > sim_6_log.txt # test conf file
+# ../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_asymmetric.conf > sim_6_log_small.txt
+../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_symmetric.conf > sim_6_log_small.txt
+
 
 mv 3task_act.txt sim_6_act_small.txt
 mv 3task_data.txt sim_6_data_small.txt
