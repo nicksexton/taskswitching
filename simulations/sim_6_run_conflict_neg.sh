b34daaca4692fe38d387d51fdaf860293ac651fb
Nick Sexton <nick.sexton@gmail.com>
2014-10-29 17:15:27 +0000

    simulation 6c, different approaches to conflict

---
+++ simulations/sim_6_run_conflict_neg.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script to run & compare 3 approaches to negative conflict: allow, clip and rescale.
+
+# ./sim_4_clean.sh
+# ./sim_6_clean.sh
+
+rm sim_4_trials.conf
+rm sim_6_trials.conf
+rm sim_4_lookup.txt
+rm sim_6_lookup.txt
+rm sim_4_data.txt
+rm sim_6_data.txt
+rm sim_4_act.txt
+rm sim_6_act.txt
+
+rm sim_6_log.txt
+
+./sim_4_asymmetric.py -n 1000
+mv sim_4_trials.conf sim_6_trials.conf
+mv sim_4_lookup.txt sim_6_lookup.txt
+
+# Allow negative conflict
+../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_symmetric_conflict_neg.conf > sim_6_log_allow.txt
+mv 3task_act.txt sim_6_act_conflict_neg_allow.txt
+mv 3task_data.txt sim_6_data_conflict_neg_allow.txt
+
+# clip negative conflict
+../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_symmetric_conflict_clip.conf > sim_6_log_clip.txt
+mv 3task_act.txt sim_6_act_conflict_neg_clip.txt
+mv 3task_data.txt sim_6_data_conflict_neg_clip.txt
+
+# rescale conflict
+../3task_basic_koch_conflict -t sim_6_trials.conf -m sim_6_model_symmetric_conflict_rescale.conf > sim_6_log_rescale.txt
+mv 3task_act.txt sim_6_act_conflict_neg_rescale.txt
+mv 3task_data.txt sim_6_data_conflict_neg_rescale.txt
---
+++ simulations/sim_6c_analysis.R
@@ -0,0 +1,323 @@
+# R data analysis for thesis simulation 4 & 6
+# analysing asymmetric switching between 3 tasks (simulation 4)
+# see Arbuthnott (2008), thesis notes on same, and lab notebook for details
+
+# simulation 6 uses same task structure as simulation 4 but with conflict monitoring model
+
+# simulation 6c - comparison of 3 approaches to dealing with negative conflict - allow, clip and rescale.
+
+# design:
+# - four types of 3-trial run: no switch (BAA, BCC), one switch (AAB, BBC), two switch (CBA, ABC),
+# alternating switch (ABA, CBC).
+# Critical trial is the 3rd trial for each run.
+# Allow assessment of relative difficulty (RTs and error rates for trial 3 of no-switch runs),
+# switch costs (two switch vs. one switch), and n-2 repetition costs (two switch vs. alternating switch).
+# switch cost asymmetry is the difference in switch costs between 
+
+# distractor stimuli for all trials are incongruent/incongruent (see simulation 3.1)
+# w.r.t. the correct response of the cued stim dimension.
+# ie., if cued task is A, stim [0 1 1] would be II and [0 0 1] would be IC.
+# these need to be counterbalanced across all 3 trials for both run types (ie., 8 conditions per run type)
+# disregarding congruent/congruent trials for now.
+
+# Basic hypothesis: Based on the hypothesis that n-2 repetition costs reflect a specific inhibition mechanism,
+# lateral inhibition only (ie., modified gilbert & shallice model used in simulation 3) will not produce
+# n-2 repetition costs
+
+# Contrasts
+# Trial 3 in CBA trials vs. trial 3 in ABA trials
+# n-2 repetition cost - RT of trial 3 significantly longer in ABA sequences 
+
+# Additional hypotheses:
+# Congruency may affect/interact with task sequence.
+
+rm (list = ls())
+library(ggplot2) # for graphs
+library(pastecs) # for descriptive statistics
+library(reshape2) # for transform
+imageDirectory <- file.path(Sys.getenv("HOME"), "Dropbox", "PhD", "Thesis", "simulation_results", "simulation_6")
+labels.data = c("trialpath", "trialid", "cue", "stim_0", "stim_1", "stim_2", "cycles",
+           "response")
+#
+data.allow <- read.delim("sim_6_data_conflict_neg_allow.txt",
+                         header=FALSE, sep=c("", ":"), col.names=labels.data)
+data.clip <- read.delim("sim_6_data_conflict_neg_clip.txt",
+                        header=FALSE, sep=c("", ":"), col.names=labels.data)
+data.rescale <- read.delim("sim_6_data_conflict_neg_rescale.txt",
+                           header=FALSE, sep=c("", ":"), col.names=labels.data)
+                                        # now split trial path into block and trial ID
+data.allow <- cbind (data.allow, conflict="allow")
+data.clip <- cbind (data.clip, conflict="clip")
+data.rescale <- cbind (data.rescale, conflict="rescale")
+data.raw <- rbind (data.allow, data.clip, data.rescale)
+
+
+data.raw$trialpath <- as.character(data.raw$trialpath)
+data.raw = transform (data.raw, PATH = colsplit(trialpath, pattern = "\\:", names=c('block', 'trial')))
+#
+data.raw$correct <- with (data.raw, ifelse (cue == 0, stim_0 == response %% 2,
+                                    ifelse (cue == 1, stim_1 == response %% 2,
+                                    ifelse (cue == 2, stim_2 == response %% 2, NA))))
+#
+#
+#
+# Join lookup table with simulated data
+labels.lookup = c("trialid", "sequence_cond", "sequence", "trial_pos", "congruency_seq", "congruency_trial", "blank")
+data.lookuptable = read.delim("sim_6_lookup.txt", header = FALSE, col.names=labels.lookup)
+data.raw <- merge(data.lookuptable, data.raw, by.x = "trialid", by.y = "trialid")
+data.raw = subset(data.raw, select = c("trialid", "sequence_cond", "sequence", "PATH.block", "PATH.trial", "cue", "stim_0", "stim_1", "stim_2", "response", "cycles", "correct", "conflict"))
+#
+# create labels for tasks making up whole sequence 
+data.raw = transform (data.raw, seq = colsplit(sequence, pattern = "/", names=c('1', '2', '3')))
+#
+# NB congruency analysis not needed for simulation 4 yet 
+#data = transform (data, congruency = colsplit(congruency_seq, pattern = "/", names=c('1', '2', '3')))
+#data$congruency.23 <- paste (data$congruency.2,  data$congruency.3, sep="/")
+#data$congruency.12 <- paste (data$congruency.1,  data$congruency.2, sep="/")
+#
+#
+data <- data.raw
+# filter trials for correct only
+                                        # should filter for correct sequences only!
+data = subset (data.raw, correct == TRUE)
+#
+# exclude outliers (RTs +/- 3 SDs) for each task (irrespective of trial position or switch condition)
+# exclude outliers (RTs +/- 3 SDs)
+descriptives <- by(data$cycles, data$cue, stat.desc)
+data <- subset (data,
+        !((cue == 0 ) & cycles > descriptives$"0"[9] + 3 * descriptives$"0"[13]))
+data <- subset (data,
+        !((PATH.trial == 1 ) & cycles > descriptives$"1"[9] + 3 * descriptives$"1"[13]))
+data <- subset (data,
+        !((PATH.trial == 2 ) & cycles > descriptives$"2"[9] + 3 * descriptives$"2"[13]))
+#
+#
+# Now look only at trial 3
+#
+#
+#
+###task 0 (easiest)
+#data.task0<- subset (data, PATH.trial == 2 & seq.3 == 0)
+#by(data.task0$cycles, data.task0$seq.2, stat.desc)
+#
+##task 1 (intermediate)
+#data.task1<- subset (data, PATH.trial == 2 & seq.3 == 1)
+#by(data.task1$cycles, data.task1$seq.2, stat.desc)
+#
+##task 2 (hardest)
+#data.task2<- subset (data, PATH.trial == 2 & seq.3 == 2)
+#by(data.task2$cycles, data.task2$seq.2, stat.desc)
+#
+#
+data$seq.3 <- factor(data$seq.3)
+data$seq.2 <- factor(data$seq.2)
+data$seq.1 <- factor(data$seq.1)
+#
+#
+#
+######################### DEBUG STUFF ####################################
+
+
+boxplot.clip <- ggplot (subset(data, PATH.trial == 2 & conflict=="clip"), aes(x=sequence_cond, y=cycles))
+boxplot.clip + geom_boxplot() + labs (x = "sequence", y = "RT (cycles)") +
+  ggtitle ("Simulation 6c, negative conflict allowed")
+
+boxplot.allow <- ggplot (subset(data, PATH.trial == 2 & conflict=="allow"), aes(x=sequence_cond, y=cycles))
+boxplot.allow + geom_boxplot() + labs (x = "sequence", y = "RT (cycles)") +
+  ggtitle ("Simulation 6c, negative conflict clipped")
+
+boxplot.rescale <- ggplot (subset(data, PATH.trial == 2 & conflict=="rescale"), aes(x=sequence_cond, y=cycles))
+boxplot.rescale + geom_boxplot() + labs (x = "sequence", y = "RT (cycles)") +
+  ggtitle ("Simulation 6c, negative conflict rescaled")
+
+
+
+##########################################################################
+##########################################################################
+##########################################################################
+
+
+
+# Plot graph for switches between tasks 0 and 1
+data.task01 <- subset (data, PATH.trial == 2 & (
+                               (seq.3 == 0 & (seq.2 == 0 | seq.2 == 1)) |
+                               (seq.3 == 1 & (seq.2 == 1 | seq.2 == 0)) )
+                       )
+
+
+# Plot graph for switches between tasks 0 and 2
+data.task02 <- subset (data, PATH.trial == 2 & (
+                               (seq.3 == 0 & (seq.2 == 0 | seq.2 == 2)) |
+                               (seq.3 == 2 & (seq.2 == 2 | seq.2 == 0)) )
+                       )
+
+
+
+# Plot graph for switches between tasks 1 and 2
+data.task12 <- subset (data, PATH.trial == 2 & (
+                               (seq.3 == 1 & (seq.2 == 1 | seq.2 == 2)) |
+                               (seq.3 == 2 & (seq.2 == 2 | seq.2 == 1)) )
+                       )
+
+linegraph <- ggplot (data.task12, aes(x=sequence_cond, y=cycles, group=seq.3, fill=seq.3))
+linegraph +
+  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
+  labs (x = "Sequence", y = "RT", group = "Sequence") +
+  ggtitle("Simulation 6: Asymmetric switch costs and n-2 repetition costs\n Switches between tasks 1 and 2")
+imageFile <- file.path(imageDirectory, "sim_6_0_tasks12.png") 
+ggsave(imageFile)
+
+
+# descriptive statistics, tasks 0 (easy) and 1 (intermediate)
+data.task01.1 <- subset(data.task01, data.task01$seq.3==1) # harder task (1)
+by(data.task01.1$cycles, data.task01.1$sequence_cond, stat.desc)
+
+data.task01.0 <- subset(data.task01, data.task01$seq.3==0) # easier task (0)
+by(data.task01.0$cycles, data.task01.0$sequence_cond, stat.desc)
+
+
+# descriptive statistics, tasks 0 (easy) and 2 (hard)
+data.task02.2 <- subset(data.task02, data.task02$seq.3==2) # hard task (2)
+by(data.task02.2$cycles, data.task02.2$sequence_cond, stat.desc)
+
+data.task02.0 <- subset(data.task02, data.task02$seq.3==0) # easy task (0)
+by(data.task02.0$cycles, data.task02.0$sequence_cond, stat.desc)
+
+# descriptive statistics, tasks 1 (intermediate) and 2 (hard)
+data.task12.2 <- subset(data.task12, data.task12$seq.3==2) # hard task (2)
+by(data.task12.2$cycles, data.task12.2$sequence_cond, stat.desc)
+
+data.task12.1 <- subset(data.task12, data.task12$seq.3==1) # easy task (1)
+by(data.task12.1$cycles, data.task12.1$sequence_cond, stat.desc)
+
+
+
+######################################### Stat Tests ######################################
+
+
+############## FOR SYMMETRIC SWITCHING ###################
+
+data.aggregate.trial3 <-subset (data, PATH.trial==2)
+by (data.aggregate.trial3$cycles, data.aggregate.trial3$sequence_cond, stat.desc) # RT on trial 3 only
+
+# t-test for switch cost
+model.aggregate.trial3.switchcost <- t.test (cycles ~ sequence_cond,
+                                             data = subset (data.aggregate.trial3,
+                                                 sequence_cond == "0SW" | sequence_cond == "1SW"))
+model.aggregate.trial3.switchcost
+
+
+# t-test for n-2 repetition cost
+model.aggregate.trial3.n2rc <- t.test (cycles ~ sequence_cond,
+                                             data = subset (data.aggregate.trial3,
+                                                 sequence_cond == "2SW" | sequence_cond == "ALT"))
+model.aggregate.trial3.n2rc
+
+
+
+############## FOR ASYMMETRIC SWITCHING ##################
+
+
+############## 0 and 1 ##################
+
+# Switch cost - assessing whether switch costs are reversed
+# 2X2 ANOVA:
+# Factors: sequence_cond (levels: 0SW, 1SW only), seq.3 (levels: 0, 2)
+# Hypothesis:
+#    main effect of sequence cond (ie., switch cost)
+#    main effect of seq.3 (ie., different RTs for different tasks)
+#    seq.3 x sequence_cond interaction: signf. asymmetric switch cost
+
+
+# switch cost
+model.task01.sc <- aov(cycles ~ sequence_cond +
+                       seq.3 + 
+                       sequence_cond:seq.3,
+                       data = subset(data.task01, sequence_cond == "0SW" | sequence_cond == "1SW"))
+anova(model.task01.sc)
+
+#n2 repetition cost
+model.task01.n2rc <- aov(cycles ~ sequence_cond +
+                       seq.3 + 
+                       sequence_cond:seq.3,
+                       data = subset(data.task01, sequence_cond == "ALT" | sequence_cond == "2SW"))
+anova(model.task01.n2rc)
+
+# post-hoc test: is there a significant n-2 repetition cost in each condition
+# task 0
+model.task01.n2rc.0 <- t.test (cycles ~ sequence_cond,
+                               data = subset (data.task01, seq.3 == 0),
+                               sequence_cond == "2SW" | sequence_cond == "ALT")
+model.task01.n2rc.0
+
+# task 1
+model.task01.n2rc.1 <- t.test (cycles ~ sequence_cond,
+                               data = subset (data.task01, seq.3 == 1),
+                               sequence_cond == "2SW" | sequence_cond == "ALT")
+model.task01.n2rc.1
+
+
+
+# Test for reversed switch cost asymmetry???
+
+
+
+############## 1 and 2 ##################
+
+# switch cost
+model.task12.sc <- aov(cycles ~ sequence_cond +
+                       seq.3 + 
+                       sequence_cond:seq.3,
+                       data = subset(data.task12, sequence_cond == "0SW" | sequence_cond == "1SW"))
+anova(model.task12.sc)
+
+#n2 repetition cost
+model.task12.n2rc <- aov(cycles ~ sequence_cond +
+                       seq.3 + 
+                       sequence_cond:seq.3,
+                       data = subset(data.task12, sequence_cond == "ALT" | sequence_cond == "2SW"))
+anova(model.task12.n2rc)
+
+# post-hoc test: is there a significant n-2 repetition cost in each condition
+# task 1
+model.task12.n2rc.1 <- t.test (cycles ~ sequence_cond,
+                               data = subset (data.task12, seq.3 == 1),
+                               sequence_cond == "2SW" | sequence_cond == "ALT")
+model.task12.n2rc.1
+
+# task 2
+model.task12.n2rc.2 <- t.test (cycles ~ sequence_cond,
+                               data = subset (data.task12, seq.3 == 2),
+                               sequence_cond == "2SW" | sequence_cond == "ALT")
+model.task12.n2rc.2
+
+############## 0 and 2 ##################
+
+# switch cost
+model.task02.sc <- aov(cycles ~ sequence_cond +
+                       seq.3 + 
+                       sequence_cond:seq.3,
+                       data = subset(data.task02, sequence_cond == "0SW" | sequence_cond == "1SW"))
+anova(model.task02.sc)
+
+#n2 repetition cost
+model.task02.n2rc <- aov(cycles ~ sequence_cond +
+                       seq.3 + 
+                       sequence_cond:seq.3,
+                       data = subset(data.task02, sequence_cond == "ALT" | sequence_cond == "2SW"))
+anova(model.task02.n2rc)
+
+# post-hoc test: is there a significant n-2 repetition cost in each condition
+# task 1
+model.task02.n2rc.0 <- t.test (cycles ~ sequence_cond,
+                               data = subset (data.task02, seq.3 == 0),
+                               sequence_cond == "2SW" | sequence_cond == "ALT")
+model.task02.n2rc.0
+
+# task 2
+model.task02.n2rc.2 <- t.test (cycles ~ sequence_cond,
+                               data = subset (data.task02, seq.3 == 2),
+                               sequence_cond == "2SW" | sequence_cond == "ALT")
+model.task02.n2rc.2
+
