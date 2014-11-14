42d8c03dea832ed4417480f65e671347fffe4ec4
Nick Sexton <nick.sexton@gmail.com>
2014-11-14 16:47:59 +0000

    modification to gridsearch init file

--- simulations/sim_6d_gridsearch_init.R
+++ simulations/sim_6d_gridsearch_init.R
@@ -6,7 +6,7 @@ filename.output.genetic.results <- "sim_6d_gridsearch_results_clip_lownoise_2.tx
 
 
 blocksize <- 100
-n <- 15 # resolution of grid to explore param space (ie total observations is n^ncols(min)
+n <- 5 # resolution of grid to explore param space (ie total observations is n^ncols(min)
 max.cycles <- 500
 model.conf.leaf.min <- c(conflict.gain = 1.0, conflict.tdwt = -30.0, conflict.bias = -40.0)
 model.conf.leaf.max <- c(conflict.gain = 100.0, conflict.tdwt = -1.0, conflict.bias = -1.0)
