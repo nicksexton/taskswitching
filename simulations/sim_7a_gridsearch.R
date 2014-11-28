e06664860066d9cd92fcfd7fbe02675de488a8e3
Nick Sexton <nick.sexton@gmail.com>
2014-11-28 17:10:08 +0000

    simulation 7, not working yet

---
+++ simulations/sim_7a_gridsearch.R
@@ -0,0 +1,300 @@
+# Executes a gridsearch over a bounded parameter space, generates a list of parameters, switch costs, and n-2 repetition costs, and reports stat tests. 
+# IMPORTANT! run create_ramdisk.sh as root before executing script to create ramdisk for temporary files
+
+# Version of the gridsearch that uses resampling to improve data from each voxel
+
+# rm (list = ls())
+
+library (reshape2) # for colsplit
+library (pastecs) # for stat.desc
+library (plyr) # for ddply
+
+path.simulation <- "/home/nickdbn/Programming/c_pdp_models/simulations/" 
+setwd (path.simulation)
+
+# source ("sim_6e_gridsearch_init.R") # init file SPECIFIC TO A RUN OF A SIMULATION
+source (paste(path.simulation, "sim_6_analysis_functions_calcerrors.R", sep=""))
+
+path.ramdisk <- paste("/media/ramdisk/", path.ramdiskfolder, "/", sep="")
+system2("mkdir", args=path.ramdisk)
+
+
+
+
+
+filename.conf.temp.stem <- "sim_7a_params_" # for parallel version
+filename.output.temp.stem <- "sim_7a_data_" # for parallel version
+filename.conf.temp <- "sim_7a_params_temp.conf" # will be created
+filename.output.data.temp <- "sim_7a_data_temp.txt"
+
+
+setwd (path.ramdisk)
+system2 ("rm", args="sim_7a_lookup.txt sim_7a_trials.conf")
+system2 (paste(path.simulation, "sim_7a_gridsearch_trials.py", sep=""), args=paste("-n", blocksize))
+
+labels.lookup = c("trialid", "sequence_cond", "sequence", "trial_pos", "congruency_seq", "congruency_trial", "blank")
+data.lookuptable = read.delim("sim_7a_lookup.txt", header = FALSE, col.names=labels.lookup)
+
+
+## Note, model.conf.stem defined in init file
+
+model.conf.makeleaf <- function (x) paste ("\nSTIMULUS_INPUT_STRENGTH_0", x$task.input.str,
+                                          "\nCONFLICT_TASKDEMAND_WT", x$conflict.tdwt,
+                                          "\nTOPDOWN_CONTROL_STRENGTH_0", x$task.topdown.str)
+
+
+model.run <- function (stem, leaf, conf.tempfile, output.tempfile) {
+
+  write (paste(stem, leaf), conf.tempfile, append=FALSE)
+  
+  run.args = paste("-t sim_7a_trials.conf -m ", conf.tempfile, sep=" ")
+  system2 (paste(path.simulation, "/../3task_basic_koch_conflict", sep=""),
+           args=run.args, stdout = NULL, stderr = NULL)
+
+  system2 ("mv", args=paste("3task_data.txt", paste(path.ramdisk, output.tempfile, sep=""))) 
+}
+
+
+data.preprocess <- function (datafile, lookuptable) {
+
+  labels.data = c("trialpath", "trialid", "cue", "stim_0", "stim_1", "stim_2", "cycles",
+           "response")
+  data <- read.delim(paste(path.ramdisk, datafile, sep=""),
+                     header=FALSE, sep=c("", ":"), col.names=labels.data)
+  data <- split.trialpath (data)
+  data <- process.data (data, max.cycles=max.cycles) # filters data for trial 3 only!
+
+  data <- merge(lookuptable, data, by.x = "trialid", by.y = "trialid")
+  data <- subset(data, select = c("trialid", "sequence_cond", "sequence", "PATH.block", "PATH.trial", "cue", "stim_0", "stim_1", "stim_2", "response", "cycles", "gaveresponse.trial", "correct.trial", "correct.block12", "gaveresponse.block12"))
+
+  
+ # split columns for processing
+  data <- transform (data, seq = colsplit(sequence, pattern = "/", names=c('1', '2', '3')))
+  data$seq.3 <- factor(data$seq.3)
+  data$seq.2 <- factor(data$seq.2)
+  data$seq.1 <- factor(data$seq.1)
+
+  return (data)
+}
+
+
+data.analyse <- function (data) {
+
+    errors.trial12 <- calculate.errors (data$correct.block12, data$sequence_cond) 
+  names(errors.trial12) <- c("err.12.0SW", "err.12.1SW", "err.12.2SW", "err.12.ALT")  
+
+  timeouts.trial12 <- calculate.errors (data$gaveresponse.block12, data$sequence_cond)
+  names(timeouts.trial12) <- c("timeout.12.0SW", "timeout.12.1SW", "timeout.12.2SW", "timeout.12.ALT")  
+                                        # now we can filter out blocks where there was an error on trials 1 or 2
+  data <- subset (data, correct.block12 == TRUE)
+  
+  
+  errors.trial3 <- calculate.errors (data$correct.trial, data$sequence_cond)
+  timeouts.trial3 <- calculate.errors (data$gaveresponse.trial, data$sequence_cond)
+  names(timeouts.trial12) <- c("timeout.3.0SW", "timeout.3.1SW", "timeout.3.2SW", "timeout.3.ALT")   
+  names(errors.trial3) <- c("err.3.0SW", "err.3.1SW", "err.3.2SW", "err.3.ALT")
+  
+                                        # filter the errors out to calc switchcost 
+  data <- subset (data, PATH.trial == 2 & correct.trial == TRUE)
+
+  
+  
+  results <- cbind (calculate.switchcost (data),
+                    calculate.n2rc(data),
+                    t(errors.trial12),
+                    t(timeouts.trial12),
+                    t(errors.trial3),
+                    t(timeouts.trial3))
+  
+  return (results)
+
+}
+
+run.individual <- function (leaf, # parameter leaf (a data frame)
+                            stem, # parameter file stem (a (long) character vector)
+                            conf.file.temp, #filename for temp conf file
+                            output.file.temp) { # filename for temp data
+
+  model.run (stem,
+             model.conf.makeleaf (leaf),
+             conf.file.temp,
+             output.file.temp)
+  data <- data.preprocess (output.file.temp, data.lookuptable)
+  # clean up output file
+  system2 ("rm", args=c(output.file.temp, conf.file.temp, "3task_act.txt")) # run.individual cleans output tempfile  
+
+
+  # How we classify trials according to which tasks the 2nd trial in each seq belongs to
+  switchdir <- data.frame(sequence=c("0/0/1", "0/0/2", "0/1/0", "0/1/1", "0/1/2", "0/2/0",
+                                     "0/2/1", "0/2/2", "1/0/0", "1/0/1", "1/0/2", "1/1/0",
+                                     "1/1/2", "1/2/0", "1/2/1", "1/2/2", "2/0/0", "2/0/1",
+                                     "2/0/2", "2/1/0", "2/1/1", "2/1/2", "2/2/0", "2/2/1"),
+                          alternation=c("AB", "AC", "BA", "AB", "BC", "CA",
+                                        "CB", "AC", "BA", "AB", "AC", "BA",
+                                        "BC", "CA", "CB", "BC", "CA", "AB",
+                                        "AC", "BA", "CB", "BC", "CA", "CB"))
+
+  data <- merge (data, switchdir)
+                                        # create an empty data frame to paste the data into
+  results <- data.frame (
+               switchdir = c("AB", "AC", "BA", "BC", "CA", "CB"),
+               mean.0SW=vector(mode="numeric", length=6),
+               mean.1SW=vector(mode="numeric", length=6),
+               sc=vector(mode="numeric", length=6),
+               sc.t=vector(mode="numeric", length=6),
+               sc.df=vector(mode="numeric", length=6),
+               sc.p=vector(mode="numeric", length=6),
+               mean.2SW=vector(mode="numeric", length=6),
+               mean.ALT=vector(mode="numeric", length=6),
+               n2rc=vector(mode="numeric", length=6),
+               n2rc.t=vector(mode="numeric", length=6),
+               n2rc.df=vector(mode="numeric", length=6),
+               n2rc.p=vector(mode="numeric", length=6),
+               err.12.0SW=vector(mode="numeric", length=6),
+               err.12.1SW=vector(mode="numeric", length=6),
+               err.12.2SW=vector(mode="numeric", length=6),
+               err.12.ALT=vector(mode="numeric", length=6),
+               timeout.12.0SW=vector(mode="numeric", length=6),
+               timeout.12.1SW=vector(mode="numeric", length=6),
+               timeout.12.2SW=vector(mode="numeric", length=6),
+               timeout.12.ALT=vector(mode="numeric", length=6),
+               err.3.0SW=vector(mode="numeric", length=6),
+               err.3.1SW=vector(mode="numeric", length=6),
+               err.3.2SW=vector(mode="numeric", length=6),
+               err.3.ALT=vector(mode="numeric", length=6),
+               timeout.3.0SW=vector(mode="numeric", length=6),
+               timeout.3.1SW=vector(mode="numeric", length=6),
+               timeout.3.2SW=vector(mode="numeric", length=6),
+               timeout.3.ALT=vector(mode="numeric", length=6))
+
+  #  browser ()
+  
+  results <- ddply (.data=data, .(alternation), .fun=data.analyse)
+  return (results)
+  
+}
+
+
+########################### Main algorithm #############################
+
+
+
+
+generate.grid <- function (n, min, max) {
+# Generates a data frame containing a multidimensional grid with n divisions per dimension, ie n^(ncol(min)) rows
+
+  interpolate.1d <- function (n, min, max) vect <- seq (from=min, to=max, length.out=n) 
+  expand.1d <- function (vect, d) as.vector(sapply(X=vect, FUN=function(x) rep(x, d)))
+  
+                                        # pre-allocate the data frame
+
+  ndims <- length(min)
+  grid <- matrix (nrow = n^ndims, ncol=ndims)
+  fill <- function (n, min, max, dimensions) {
+    if (dimensions == 1) return (interpolate.1d (n, min, max))
+    else return (expand.1d (fill(n, min, max, dimensions-1), n))
+  }
+
+  for (dimension in 1:ndims) {
+    grid[,dimension] <- fill (n, min[dimension], max[dimension], dimension)
+  }
+
+  grid <- data.frame (grid)
+  colnames (grid) <- names(min)
+
+  return (grid)
+
+}
+
+
+
+test.population <- function (pop) {
+
+  progress <- txtProgressBar (min=0, max=nrow(pop), style=3)
+
+  for (i in 1:nrow(pop)) {
+
+    conf <- paste (filename.conf.temp.stem, ".", i, ".conf", sep="")
+    data <- paste (filename.output.temp.stem, ".", i, ".txt", sep="")
+    
+    pop[i,names(results)] <- run.individual (leaf=pop[i,names(model.conf.leaf.min)],
+                                             stem=model.conf.stem,
+                                             conf.file.temp=conf,
+                                             output.file.temp=data)
+
+    setTxtProgressBar(progress, i)
+  }
+
+  close (progress)
+  
+  return (pop)
+
+}
+
+
+
+run <- function (pop) {
+
+  pop <- cbind (pop, results)
+  pop <- test.population (pop)
+
+  file <- paste(path.simulation, filename.output.results, sep="")
+  
+  # print here
+  print (pop)
+
+  write.table (format(pop, digits=4),
+               file,
+               sep="\t",
+               append=TRUE,
+               quote=FALSE,
+               col.names=TRUE)
+  write ("\n", file, append=TRUE)
+         
+  return (pop)
+}
+
+
+
+
+
+
+params <- names (model.conf.leaf.min)
+
+results <- data.frame (mean.0SW=numeric(n),
+                       mean.1SW=numeric(n),
+                       sc=numeric(n),
+                       sc.t=numeric(n),
+                       sc.df=numeric(n),
+                       sc.p=numeric(n),
+                       mean.2SW=numeric(n),
+                       mean.ALT=numeric(n),
+                       n2rc=numeric(n),
+                       n2rc.t=numeric(n),
+                       n2rc.df=numeric(n),
+                       n2rc.p=numeric(n),
+                       err.12.0SW=numeric(n),
+                       err.12.1SW=numeric(n),
+                       err.12.2SW=numeric(n),
+                       err.12.ALT=numeric(n),
+                       timeout.12.0SW=numeric(n),
+                       timeout.12.1SW=numeric(n),
+                       timeout.12.2SW=numeric(n),
+                       timeout.12.ALT=numeric(n),
+                       err.3.0SW=numeric(n),
+                       err.3.1SW=numeric(n),
+                       err.3.2SW=numeric(n),
+                       err.3.ALT=numeric(n),
+                       timeout.3.0SW=numeric(n),
+                       timeout.3.1SW=numeric(n),
+                       timeout.3.2SW=numeric(n),
+                       timeout.3.ALT=numeric(n)
+                       )
+
+grid <- generate.grid (n,
+                       model.conf.leaf.min,
+                       model.conf.leaf.max)
+                                        # include alternation to force grid to create space in results table
+
+
+  grid <- run (grid)
---
+++ simulations/sim_7a_gridsearch_clip_init.R
@@ -0,0 +1,59 @@
+# Initialisation file for specific run of a simulation
+# noise at .006 (default) for comparison of errors, c/w .004 for previous gridsearch
+
+rm (list = ls())
+
+path.simulation <- "/home/nickdbn/Programming/c_pdp_models/simulations/" 
+setwd (path.simulation)
+
+
+
+filename.output.results <- "sim_7a_gridsearch_results_highnoise_clip.txt" # where to store results
+
+
+blocksize <- 50
+n <- 7 # resolution of grid to explore param space (ie total observations is n^ncols(min)
+max.cycles <- 500
+
+# Bounds for gridsearch
+model.conf.leaf.min <- c(task.input.str = 1.5, task.topdown.str = 6.0, conflict.tdwt = -30.0)
+model.conf.leaf.max <- c(task.input.str = 5.0, task.topdown.str = 20.0, conflict.tdwt = 0.0)
+
+
+path.ramdiskfolder <- "7a_clip_0"
+
+# define stem in init file
+model.conf.stem = (" 
+ACTIVATION_MAX 1.0 
+ACTIVATION_MIN -1.0 
+RESPONSE_THRESHOLD 0.15 
+STEP_SIZE 0.0015 
+SQUASHING_PARAM 0.8 
+CONFLICT_SQUASHING_PARAM 0.5 
+NOISE 0.006 # default .006
+HEBBIAN_LEARNING_PERSISTENCE 1 
+RSI_SCALE_PARAM 1.0 
+BIAS_OUTPUTUNIT -6.0 
+BIAS_TASKDEMAND -4.0 
+BIAS_NONE 0 
+BIAS_CONFLICT -10.0 
+CONFLICT_GAIN 75.0 
+STIMULUS_INPUT_STRENGTH_1 3.0 
+STIMULUS_INPUT_STRENGTH_2 3.0 
+TASKDEMAND_OUTPUT_INHIBITORY_WT -2.5 
+TASKDEMAND_OUTPUT_EXCITATORY_WT 2.5 
+TASKDEMAND_LATERAL_INHIBITORY_WT -2.0 
+CONFLICT_NEGATIVE 2 # clip
+TOPDOWN_CONTROL_STRENGTH_1 12.0  
+TOPDOWN_CONTROL_STRENGTH_2 12.0 
+LEARNING_RATE 1.0 
+MAX_CYCLES 500") # tailing spaces eliminates double newlines, for some reason
+                                        # Leaf parameters: 
+                                        # STIMULUS_INPUT_STRENGTH_0 3.0 
+                                        # TOPDOWN_CONTROL_STRENGTH_0 12.0 
+                                        # CONFLICT_TASKDEMAND_WT -14.0 
+
+
+
+
+source ("sim_7a_gridsearch.R")
