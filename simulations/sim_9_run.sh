cddd4a46b82814e24a79823b15e166298ce3c9ac
Nick Sexton <nick.sexton@gmail.com>
2015-03-10 08:14:37 +0000

    initial script and conf files for simulation 9

---
+++ simulations/sim_9_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./sim_3_clean.sh
+rm sim_9_data.txt
+rm sim_9_log.txt
+
+./sim_3_n2rc.py -n 100
+
+# ../3task_basic -t sim_3_trials.conf -m sim_9_model_symmetric.conf > sim_9_log.txt
+../3task_model_koch -t sim_3_trials.conf -m sim_9_model_symmetric.conf > sim_9_log.txt
+
+# mv 3task_act.txt sim_9_act.txt
+rm 3task_act.txt 
+mv 3task_data.txt sim_9_data.txt
