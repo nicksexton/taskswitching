a113f0e469a0077826dcfb1c89243d54713ff6f0
Nick Sexton <nick.sexton@gmail.com>
2015-06-24 15:27:24 +0100

    model produces effect for tasks 0-1, 0-2 only

--- simulations/sim_10_analysis.R
+++ simulations/sim_10_analysis.R
@@ -47,41 +47,36 @@ data.raw <- read.delim("sim_10_data.txt", header=FALSE, sep=c("", ":"), col.name
                                         # now split trial path into block and trial ID
 data.raw$trialpath <- as.character(data.raw$trialpath)
 data.raw = transform (data.raw, PATH = colsplit(trialpath, pattern = "\\:", names=c('block', 'trial')))
-#
-data.raw$correct <- with (data.raw, ifelse (cue == 0, stim_0 == response %% 2,
-                                    ifelse (cue == 1, stim_1 == response %% 2,
-                                    ifelse (cue == 2, stim_2 == response %% 2, NA))))
-#
+
+
+                                        #
 #
 #
 # Join lookup table with simulated data
 labels.lookup = c("trialid", "sequence_cond", "sequence", "trial_pos", "congruency_seq", "congruency_trial", "blank")
-data.lookuptable = read.delim("sim_6_lookup.txt", header = FALSE, col.names=labels.lookup)
+data.lookuptable = read.delim("sim_10_lookup.txt", header = FALSE, col.names=labels.lookup)
 data.raw <- merge(data.lookuptable, data.raw, by.x = "trialid", by.y = "trialid")
-data.raw = subset(data.raw, select = c("trialid", "sequence_cond", "sequence", "PATH.block", "PATH.trial", "cue", "stim_0", "stim_1", "stim_2", "response", "cycles", "correct"))
+data.raw = subset(data.raw, select = c("trialid", "sequence_cond", "sequence", "PATH.block", "PATH.trial", "cue", "stim_0", "stim_1", "stim_2", "response", "cycles"))
 #
 # create labels for tasks making up whole sequence 
 data.raw = transform (data.raw, seq = colsplit(sequence, pattern = "/", names=c('1', '2', '3')))
-#
-# NB congruency analysis not needed for simulation 4 yet 
-#data = transform (data, congruency = colsplit(congruency_seq, pattern = "/", names=c('1', '2', '3')))
-#data$congruency.23 <- paste (data$congruency.2,  data$congruency.3, sep="/")
-#data$congruency.12 <- paste (data$congruency.1,  data$congruency.2, sep="/")
-#
-#
-data <- data.raw
-# filter trials for correct only
-                                        # should filter for correct sequences only!
-# data = subset (data.raw, correct == TRUE)
+
+# Sort out correctness
+data.raw$correct <- with (data.raw, ifelse (cue == 0, stim_0 == response %% 2,
+                                    ifelse (cue == 1, stim_1 == response %% 2,
+                                    ifelse (cue == 2, stim_2 == response %% 2, NA))))
+data.raw$correct.nminus1[2:nrow(data.raw)] <- data.raw$correct[1:nrow(data.raw) - 1]
+data.raw$correct.nminus2[2:nrow(data.raw)] <- data.raw$correct.nminus1[1:nrow(data.raw) - 1]
 
 
-  
-data.raw$exclude <- apply (data.raw, 1, function (x) all (data.raw[data.raw$PATH.block==x["PATH.block"],]$correct))
+# Filter for trial 3, correct sequences only
+data <- subset (data.raw, data.raw$PATH.trial == 2 &
+                data.raw$correct == TRUE & 
+                data.raw$correct.nminus1 == TRUE & 
+                data.raw$correct.nminus2 == TRUE)
 
-data.raw$cue.prev <- as.factor(c(NA, as.character(data.raw$cue[1:nrow(data.raw)-1])))
-data.correct <- subset (data.raw, data.raw$exclude == FALSE)
 
-
+# Don't exclude outliers for rt distribution analysis??
 
                                         #
 # exclude outliers (RTs +/- 3 SDs) for each task (irrespective of trial position or switch condition)
@@ -95,21 +90,22 @@ data.correct <- subset (data.raw, data.raw$exclude == FALSE)
 #        !((PATH.trial == 2 ) & cycles > descriptives$"2"[9] + 3 * descriptives$"2"[13]))
 
 
+
 # Now look only at trial 3
 #
 #
 #
 ###task 0 (easiest)
-#data.task0<- subset (data, PATH.trial == 2 & seq.3 == 0)
-#by(data.task0$cycles, data.task0$seq.2, stat.desc)
+data.task0<- subset (data, seq.3 == 0)
+by(data.task0$cycles, data.task0$seq.2, stat.desc)
 #
 ##task 1 (intermediate)
-#data.task1<- subset (data, PATH.trial == 2 & seq.3 == 1)
-#by(data.task1$cycles, data.task1$seq.2, stat.desc)
+data.task1<- subset (data, seq.3 == 1)
+by(data.task1$cycles, data.task1$seq.2, stat.desc)
 #
 ##task 2 (hardest)
-#data.task2<- subset (data, PATH.trial == 2 & seq.3 == 2)
-#by(data.task2$cycles, data.task2$seq.2, stat.desc)
+data.task2<- subset (data, seq.3 == 2)
+by(data.task2$cycles, data.task2$seq.2, stat.desc)
 #
 #
 
@@ -119,96 +115,6 @@ data$seq.1 <- factor(data$seq.1)
 #
 #
 #
-######################### DEBUG STUFF ####################################
-
-# 0SW
-data.0SW <- subset (data, sequence_cond == "0SW")
-bargraph <- ggplot (subset(data.0SW, PATH.trial == 2), aes(x=seq.3, y=cycles, group=seq.1, fill=seq.1))
-bargraph +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
-  labs (x = "Task (trials 2 & 3)", y = "RT", group = "task (trial 1)") +
-  ggtitle("Simulation 4: 0SW only")
-
-
-
-# RTs for 1SW condition
-data.1SW <- subset (data, sequence_cond == "1SW")
-by (data.1SW$cycles, data.1SW$seq.3, stat.desc)
-
-bargraph <- ggplot (subset(data.1SW, PATH.trial == 2), aes(x=seq.3, y=cycles, group=seq.2, fill=seq.2))
-bargraph +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
-  labs (x = "Task (trial 3)", y = "RT", group = "task (trial 2)") +
-  ggtitle("Simulation 4: 1SW only")
-
-#look at middle trial
-bargraph <- ggplot (subset(data.1SW, PATH.trial == 1), aes(x=seq.2, y=cycles, group=seq.1, fill=seq.1))
-bargraph +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
-  labs (x = "Task (trial 2)", y = "RT", group = "task (trial 1)") +
-  ggtitle("Simulation 4: 1SW only, middle trial")
-
-
-#================ RTs for 2SW condition
-data.2SW <- subset (data, sequence_cond == "2SW")
-by (data.2SW$cycles, data.2SW$seq.3, stat.desc)
-bargraph <- ggplot (subset(data.2SW, PATH.trial ==1), aes(x=seq.3, y=cycles, group=seq.2, fill=seq.2))
-bargraph +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
-  labs (x = "Task (trial 3)", y = "RT", group = "task (trial 2)") +
-  ggtitle("Simulation 4: 2SW only")
-
-#look at middle trial
-bargraph <- ggplot (subset(data.2SW, PATH.trial == 1), aes(x=seq.2, y=cycles, group=seq.1, fill=seq.1))
-bargraph +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
-  labs (x = "Task (trial 2)", y = "RT", group = "task (trial 1)") +
-  ggtitle("Simulation 4: 2SW only, middle trial")
-
-# Look at initial trial (broken out by task.sequence
-bargraph <- ggplot (subset(data.2SW, PATH.trial == 0), aes(x=seq.1, y=cycles, group=sequence, fill=sequence))
-bargraph +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
-  labs (x = "Task (trial 0)", y = "RT", group = "sequence") +
-  ggtitle("Simulation 4: 2SW only, initial trial")
-
-
-
-#=============== RTs for ALT condition
-data.ALT <- subset (data, sequence_cond == "ALT")
-by (data.ALT$cycles, data.ALT$seq.3, stat.desc)
-bargraph <- ggplot (data.ALT, aes(x=seq.3, y=cycles, group=seq.2, fill=seq.2))
-bargraph +
-  stat_summary(fun.y = mean, geom = "bar", position = "dodge") +
-  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
-  labs (x = "Task (trial 3)", y = "RT", group = "task (trial 2)") +
-  ggtitle("Simulation 4: Alt Switch only")
-
-
-boxplot.task0 <- ggplot (subset(data, PATH.trial == 2 & seq.3 == 0), aes(x=sequence_cond, y=cycles))
-boxplot.task0 + geom_boxplot() + labs (x = "sequence", y = "RT (cycles)") + ggtitle ("Simulation 6, task 0")
-#imageFile <- file.path(imageDirectory, "sim_6d_diagnostic_task0_boxplot.png") 
-#ggsave(imageFile)
-
-boxplot.task1 <- ggplot (subset(data, PATH.trial == 2 & seq.3 == 1), aes(x=sequence_cond, y=cycles))
-boxplot.task1 + geom_boxplot() + labs (x = "sequence", y = "RT (cycles)") + ggtitle ("Simulation 6, task 1")
-#imageFile <- file.path(imageDirectory, "sim_6d_diagnostic_task1_boxplot.png") 
-#ggsave(imageFile)
-
-boxplot.task2 <- ggplot (subset(data, PATH.trial == 2 & seq.3 == 2), aes(x=sequence_cond, y=cycles))
-boxplot.task2 + geom_boxplot() + labs (x = "sequence", y = "RT (cycles)") + ggtitle ("Simulation 6, task 2")
-#imageFile <- file.path(imageDirectory, "sim_6d_diagnostic_task2_boxplot.png") 
-#ggsave(imageFile)
-
-
-##########################################################################
-
 
 
 
@@ -224,8 +130,8 @@ linegraph +
   stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
   labs (x = "Sequence", y = "RT", group = "Sequence") +
   ggtitle("Simulation 6: Asymmetric switch costs and n-2 repetition costs\n Switches between tasks 0 and 1")
-imageFile <- file.path(imageDirectory, "sim_6_0_tasks01.png") 
-ggsave(imageFile)
+#imageFile <- file.path(imageDirectory, "sim_6_0_tasks01.png") 
+#ggsave(imageFile)
 
 
 
@@ -241,8 +147,8 @@ linegraph +
   stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
   labs (x = "Sequence", y = "RT", group = "Sequence") +
   ggtitle("Simulation 6: Asymmetric switch costs and n-2 repetition costs\n Switches between tasks 0 and 2")
-imageFile <- file.path(imageDirectory, "sim_6_0_tasks02.png") 
-ggsave(imageFile)
+#imageFile <- file.path(imageDirectory, "sim_6_0_tasks02.png") 
+#ggsave(imageFile)
 
 
 # Plot graph for switches between tasks 1 and 2
@@ -257,8 +163,13 @@ linegraph +
   stat_summary(fun.data = mean_cl_boot, geom = "errorbar", position = position_dodge(width = 0.90), width = 0.2) + 
   labs (x = "Sequence", y = "RT", group = "Sequence") +
   ggtitle("Simulation 6: Asymmetric switch costs and n-2 repetition costs\n Switches between tasks 1 and 2")
-imageFile <- file.path(imageDirectory, "sim_6_0_tasks12.png") 
-ggsave(imageFile)
+#imageFile <- file.path(imageDirectory, "sim_6_0_tasks12.png") 
+#ggsave(imageFile)
+
+
+### Inspect error rates
+with (data.raw, table (cue, correct))
+
 
 
 # descriptive statistics, tasks 0 (easy) and 1 (intermediate)
