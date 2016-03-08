ec6075b287553fc3bde8891da5c1961b3f435ea4
Nick Sexton <nick.sexton@gmail.com>
2016-03-08 16:57:02 +0000

    sim_16_trials.py now produces trials.conf file with longer runs of trials needed for strat adapt simulation

---
+++ simulations/sim_16_gui_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+../3task_gui_koch_conflict -t sim_16_trials.conf -m sim_14_model_symmetric.conf 
+
---
+++ simulations/sim_16_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ./sim_3_clean.sh
+rm sim_16_data.txt
+rm sim_16_log.txt
+rm sim_16_trials.conf
+rm sim_16_lookup.txt
+
+./sim_16_trials.py -n 10 -t 20 # use 100 for full simulation
+
+# ../3task_basic -t sim_3_trials.conf -m sim_9_model_symmetric.conf > sim_9_log.txt
+../3task_basic_koch_conflict -t sim_16_trials.conf -m sim_15_model_symmetric.conf > sim_16_log.txt
+
+# mv 3task_act.txt sim_9_act.txt
+rm 3task_act.txt 
+mv 3task_data.txt sim_16_data.txt
