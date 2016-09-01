bcfcb28aade265fba4b8b6453dcba0c48f2329ae
Nick Sexton <nick.sexton@gmail.com>
2016-09-01 08:05:04 +0100

    ?

--- simulations/sim_18_run.sh
+++ simulations/sim_18_run.sh
@@ -12,5 +12,13 @@ rm sim_18_lookup.txt
 ../3task_basic_koch_conflict -t sim_18_trials.conf -m sim_18_model.conf > sim_18_log.txt
 
 
-rm 3task_act.txt m
-v 3task_data.txt sim_18_data.txt
+rm 3task_act.txt
+mv 3task_data.txt sim_18_data_inhib.txt
+
+
+
+../3task_basic_koch_conflict -t sim_18_trials.conf -m sim_18_model_noinhib.conf > sim_18_log.txt
+
+
+rm 3task_act.txt
+mv 3task_data.txt sim_18_data_noinhib.txt
