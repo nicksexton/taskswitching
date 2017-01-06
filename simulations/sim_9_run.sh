61de6fe6117913578598ab5cd4b234182f29d8af
Nick Sexton <nick.sexton@gmail.com>
2017-01-06 12:26:29 +0000

    run script

--- simulations/sim_9_run.sh
+++ simulations/sim_9_run.sh
@@ -4,7 +4,7 @@
 rm sim_9_data.txt
 rm sim_9_log.txt
 
-./sim_3_n2rc_DIAGNOSTIC.py -n 200 # use 100 for full simulation
+./sim_3_n2rc_DIAGNOSTIC.py -n 100 # use 100 for full simulation
 
 # ../3task_basic -t sim_3_trials.conf -m sim_9_model_symmetric.conf > sim_9_log.txt
 ../3task_basic_koch_conflict -t sim_3_trials.conf -m sim_9_model_symmetric.conf > sim_9_log.txt
